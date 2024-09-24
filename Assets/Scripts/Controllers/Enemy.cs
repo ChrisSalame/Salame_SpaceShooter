d7ca3e4c507a59471dae757fb77c131fdf464e21
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2024-09-24 19:42:32 -0400

    task 2 is finished
    The enemy ship moves on its own and it can be adjusted in the editor

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Codice.Client.BaseCommands.CheckIn.Progress;
 
 public class Enemy : MonoBehaviour
 {
 
+    public float speed;
+
 
     private void Update()
     {
+        transform.position = new Vector2 (transform.position.x + speed, transform.position.y) ;
 
 
+
+        if (transform.position.x >= 20) 
+        {
+            speed = -speed;
+        }
+        if (transform.position.x <= -20)
+        {
+            speed = -speed;
+        }
 
     }
 
